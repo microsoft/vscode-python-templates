5d80b059a1ea91b315c4b7623b9924d836d6d217
Courtney Webster <clw99@swbell.net>
2024-05-21 17:05:30 -0500

    Expanding package example

--- package-template/my_package/__main__.py
+++ package-template/my_package/__main__.py
@@ -1,7 +1,42 @@
 from datetime import datetime
 
+class Developer:
+    valid_languages = [
+        "Python", 
+        "Java", 
+        "JavaScript", 
+        "C", 
+        "C++", 
+        "C#", 
+        "PHP", 
+        "Swift", 
+        "Go", 
+        "Kotlin", 
+        "Ruby", 
+        "Rust", 
+        "TypeScript", 
+        "Scala", 
+        "Perl", 
+        "Lua", 
+        "Groovy", 
+        "R", 
+        "Shell", 
+        "Objective-C", 
+        "SQL", 
+        "HTML/CSS"
+    ]
+    
+    def __init__(self, name, language):
+        if language not in self.valid_languages:
+            raise ValueError(f"{language} is not a valid language.")
+        self.name = name
+        self.language = language
+
+    def get_info(self):
+        return f"{self.name} is a developer who codes in {self.language}."
+
 def start_coding():
-    print("Add more Python code to this script to extend functionality!")
+    print("Start coding in Python today!")
 
 def date():
     current_datetime = datetime.now()
@@ -10,5 +45,8 @@ def date():
 def main():
     start_coding()
     print(date())
+    dev = Developer("Alice", "Python")
+    print(dev.get_info())
+    
 
 main()
